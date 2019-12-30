cdf205b81dc341d19d0ee4b0d8f0a70078b75b5f
amorCui <43329030+amorCui@users.noreply.github.com>
2019-12-31 02:53:10 +0800

    2019-12-31 version1 build

---
+++ build/precache-manifest.03fdfac4324b7b883b9d18c7eafca572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a28f6087075f3fb6aeae5b0b10179b84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1b7294388d6fdee2e44",
+    "url": "/static/css/main.9634bc41.chunk.css"
+  },
+  {
+    "revision": "13e2406ad12eaf76cfee",
+    "url": "/static/js/2.3bea1f4d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.3bea1f4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1b7294388d6fdee2e44",
+    "url": "/static/js/main.34f03fbd.chunk.js"
+  },
+  {
+    "revision": "6970778ce5c61f8567d9",
+    "url": "/static/js/runtime-main.033da1cd.js"
+  },
+  {
+    "revision": "80656b31dabbfc4496d1897c1cab2ee6",
+    "url": "/static/media/wall.80656b31.jpg"
+  }
+]);
